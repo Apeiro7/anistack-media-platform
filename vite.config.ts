5b70d5ff6bb23873019019ac26978dcdede78b65
Amit Bhardwaj <96241046+Apeiro7@users.noreply.github.com>
2026-04-16 12:24:07 +0530

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/anistack-media-platform/",
   plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
